3692d52260fdd07ec87e06f284c30c9d31943005
Zamper <ventas@zampe.cl>
2019-09-29 19:39:13 -0300

    Agregar archivos de proyecto.

---
+++ src/Tps22994.cpp
@@ -0,0 +1,3 @@
+#include "Tps22994.h"
+
+
---
+++ src/Tps22994.h
@@ -0,0 +1,12 @@
+#ifndef _Tps22994_h
+#define _Tps22994_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
