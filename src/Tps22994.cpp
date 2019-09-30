94cdedf6857f145a770cce6bb6959e5783250029
Zamper <ventas@zampe.cl>
2019-09-29 21:16:04 -0300

    primea version sin probar

--- src/Tps22994.cpp
+++ src/Tps22994.cpp
@@ -1,3 +1,61 @@
 #include "Tps22994.h"
+#include <Wire.h>
+void tps22994::channel_off(uint8_t channel)
+{
+	uint8_t ctrl_reg;
 
+	ctrl_reg = read_byte(TPS22994_REG_CONTROL);
+	ctrl_reg &= ~(1 << (channel & 0xF));
+	write_byte(TPS22994_REG_CONTROL, ctrl_reg);
+}
+void tps22994::channel_on(uint8_t channel)
+{
+	uint8_t ctrl_reg;
 
+	ctrl_reg = read_byte(TPS22994_REG_CONTROL);
+	ctrl_reg |= (1 << (channel & 0xF));
+	write_byte(TPS22994_REG_CONTROL, ctrl_reg);
+}
+
+void tps22994::channel_toggle(uint8_t channel)
+{
+	uint8_t ctrl_reg;
+
+	ctrl_reg = read_byte(TPS22994_REG_CONTROL);
+	ctrl_reg ^= (1 << (channel & 0xF));
+	write_byte(TPS22994_REG_CONTROL, ctrl_reg);
+}
+
+void tps22994::write_ctrl_reg(uint8_t value)
+{
+	write_byte(TPS22994_REG_CONTROL, value);
+}
+
+uint8_t tps22994::read_ctrl_reg()
+{
+	return read_byte(TPS22994_REG_CONTROL);
+}
+
+void tps22994::set_address(uint8_t address_)
+{
+	address = address_;
+}
+
+void tps22994::write_byte(uint8_t reg, uint8_t data)
+{
+	Wire.beginTransmission(address);
+	Wire.write(reg);
+	Wire.write(data);
+	Wire.endTransmission();
+}
+
+uint8_t tps22994::read_byte(uint8_t reg)
+{
+	////uint8_t data = 0;
+
+	Wire.beginTransmission(address);
+	Wire.write(reg);
+	Wire.endTransmission();
+	Wire.requestFrom(reg, (byte)1);
+	return Wire.read();
+}
--- src/Tps22994.h
+++ src/Tps22994.h
@@ -7,6 +7,38 @@
 	#include "WProgram.h"
 #endif
 
+#define TPS22994_I2C_ADDRESS        (0xE0>>1)
+#define TPS22994_REG_CONTROL        0x05
 
+class tps22994
+{
+public:
+//void channel_off(uint8_t address, uint8_t channel); 
+void channel_off(uint8_t channel);
+
+//void channel_on(uint8_t address, uint8_t channel);
+void channel_on(uint8_t channel);
+
+void channel_toggle(uint8_t channel);
+//void channel_toggle(uint8_t address, uint8_t channel);
+
+
+void write_ctrl_reg(uint8_t value);
+/*!
+	@brief retorna el registro de control
+	@return uint8_t 
+*/
+uint8_t read_ctrl_reg();
+
+void set_address(uint8_t address_);
+
+private:
+uint8_t address= TPS22994_I2C_ADDRESS;
+//uint8_t read_reg(uint8_t address, uint8_t reg);
+//void write_reg(uint8_t address, uint8_t reg, uint8_t value);
+
+void write_byte(uint8_t reg, uint8_t data);
+uint8_t read_byte(uint8_t reg);
+};
 #endif
 
